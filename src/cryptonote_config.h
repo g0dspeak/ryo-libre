ee8de92b7aa215908529f66af33b1d1bc70ba379
g0dspeak <letsmbpk9@gmail.com>
2024-05-14 04:43:50 -0600

    1

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -227,7 +227,7 @@ struct common_config
 
 	///////////////// Dev fund constants
 	// 2 out of 3 multisig address held by fireice, mosu, and psychocrypt
-	static constexpr const char* DEV_FUND_ADDRESS = "RYoLsiXYpuuDdf4dGNbrbZhfVYNjFTMf9CWCRwxykHx6Ahtaw5Vn5uyC59j1FqG8woi8NhBi8rNbdRmD3L81hBgMUB8ckv4t1JM";
+	static constexpr const char* DEV_FUND_ADDRESS = "RYoLshTYzNEaizPi9jWtRtNPtan5fAqc3TVbyzZDWghLY99eXivKD1XQMseVyJQ1kD5FXDvk4nHqpUXBTMCm5aqmQU8tHaN51Wc";
 	// 34d6b7155d99da44c3a73424c60ecb0da53d228ed8da026df00ed275ea54e803
 	static constexpr const char* DEV_FUND_VIEWKEY = "\x34\xd6\xb7\x15\x5d\x99\xda\x44\xc3\xa7\x34\x24\xc6\x0e\xcb\x0d\xa5\x3d\x22\x8e\xd8\xda\x02\x6d\xf0\x0e\xd2\x75\xea\x54\xe8\x03";
 	// Exact number of coins burned in the premine burn, in atomic units
