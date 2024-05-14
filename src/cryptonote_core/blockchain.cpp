c7c6eca3b993ec1957b442ea155da4ced74ab2d8
g0dspeak <letsmbpk9@gmail.com>
2024-05-14 08:58:02 -0600

    dev

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -305,20 +305,20 @@ bool Blockchain::init(BlockchainDB *db, const network_type nettype, bool offline
 
 	//memcpy(m_dev_view_key.data, common_config::DEV_FUND_VIEWKEY, 32);
 	
-	address_parse_info dev_addr;
-	if(!get_account_address_from_str<MAINNET>(dev_addr, std::string(common_config::DEV_FUND_ADDRESS)))
-	{
-		LOG_ERROR("Failed to parse dev address");
-		return false;
+	//address_parse_info dev_addr;
+	//if(!get_account_address_from_str<MAINNET>(dev_addr, std::string(common_config::DEV_FUND_ADDRESS)))
+	//{
+	//	LOG_ERROR("Failed to parse dev address");
+	//	return false;
 	}
 
-	m_dev_spend_key = dev_addr.address.m_spend_public_key;
-	crypto::public_key vk;
-	if(!secret_key_to_public_key(m_dev_view_key, vk) || vk != dev_addr.address.m_view_public_key)
-	{
-		LOG_ERROR("Dev private view key failed verification!");
-		return false;
-	}
+	//m_dev_spend_key = dev_addr.address.m_spend_public_key;
+	//crypto::public_key vk;
+	//if(!secret_key_to_public_key(m_dev_view_key, vk) || vk != dev_addr.address.m_view_public_key)
+	//{
+		//LOG_ERROR("Dev private view key failed verification!");
+	//	return false;
+	//}
 
 	if(db == nullptr)
 	{
